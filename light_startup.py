2374ea6ff2b5a06d796e6f34d9cd0a406223a6e1
cactusguy <32807678+cactusguy@users.noreply.github.com>
2017-10-15 15:33:04 -0500

    Update light_startup.py

--- light_startup.py
+++ light_startup.py
@@ -3,7 +3,7 @@ import time
 while True:
     automationhat.relay.two.on()
     print ("1")
-    time.sleep(28800)
+    time.sleep(3600)
     automationhat.relay.two.off()
     print ("2")
-    time.sleep(57600)
+    time.sleep(7200)
