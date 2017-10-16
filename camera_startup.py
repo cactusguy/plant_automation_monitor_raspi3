3a3663f96e906c629e323babf0016855d56ef71c
cactusguy <32807678+cactusguy@users.noreply.github.com>
2017-10-15 22:08:24 -0500

    Update camera_startup.py

--- camera_startup.py
+++ camera_startup.py
@@ -3,6 +3,6 @@ import time
 import picamera
 while True:
     camera = picamera.PiCamera()
-    for i in range(10):
+    for i in range(504):
         camera.capture('image{0:04d}.jpg'.format(i))
     time.sleep(3600)
